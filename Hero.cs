dbe6895f353cefc204ccf3473d43a7f1b7967b4e
EugeneLazakovich <50924111+EugeneLazakovich@users.noreply.github.com>
2019-07-28 13:42:47 +0100

    fix

--- Hero.cs
+++ Hero.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace _13._1
-{
+{   
     abstract class Hero
     {
         protected string name;
@@ -16,7 +16,8 @@ namespace _13._1
             this.intel = intel;
             this.agil = agil;
             this.stren = stren;
-        }
+        }       
+
         public override string ToString()
         {
             return "Name: " + name + "; Intelligence: " + intel + "; Agility: " + agil + "; Strenght: " + stren;
@@ -26,50 +27,35 @@ namespace _13._1
     {
         public Doom(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Doom";
-            intel = 23;
-            agil = 14;
-            stren = 31;
-        }
+            
+        }       
     }
     class Huskar : Hero
     {
         public Huskar(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Huskar";
-            intel = 15;
-            agil = 16;
-            stren = 35;
+           
         }
     }
     class Medusa : Hero
     {
         public Medusa(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Medusa";
-            intel = 16;
-            agil = 33;
-            stren = 19;
+            
         }
     }
     class Viper : Hero
     {
         public Viper(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Viper";
-            intel = 16;
-            agil = 29;
-            stren = 20;
+            
         }
     }
     class Lich : Hero
     {
         public Lich(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Lich";
-            intel = 33;
-            agil = 18;
-            stren = 20;
+            
         }
     }
 }
--- Item.cs
+++ Item.cs
@@ -26,100 +26,70 @@ namespace _13._1
     {
         public MagicStick(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Magic Stick";
-            intel = 10;
-            agil = 12;
-            stren = 6;
+            
         }
     }
     class WindLace : Item
     {
         public WindLace(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Wind Lace";
-            intel = 8;
-            agil = 14;
-            stren = 9;
+            
         }
     }
     class RingRegen : Item
     {
         public RingRegen(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Ring of Regen";
-            intel = 6;
-            agil = 15;
-            stren = 10;
+            
         }
     }
     class SageMask : Item
     {
         public SageMask(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Sage's Mask";
-            intel = 13;
-            agil = 10;
-            stren = 7;
+            
         }
     }
     class BootsSpeed : Item
     {
         public BootsSpeed(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Boots of Speed";
-            intel = 7;
-            agil = 15;
-            stren = 8;
+            
         }
     }
     class GlovesHaste : Item
     {
         public GlovesHaste(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Gloves of Haste";
-            intel = 7;
-            agil = 10;
-            stren = 13;
+            
         }
     }
     class Cloak : Item
     {
         public Cloak(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Cloak";
-            intel = 10;
-            agil = 13;
-            stren = 4;
+            
         }
     }
     class RingTarrasque : Item
     {
         public RingTarrasque(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Ring of Tarrasque";
-            intel = 9;
-            agil = 10;
-            stren = 13;
+            
         }
     }
     class RingHealth : Item
     {
         public RingHealth(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Ring of Health";
-            intel = 10;
-            agil = 12;
-            stren = 9;
+            
         }
     }
     class VoidStone : Item
     {
         public VoidStone(string name, int intel, int agil, int stren) : base(name, intel, agil, stren)
         {
-            name = "Void Stone";
-            intel = 7;
-            agil = 15;
-            stren = 11;
+            
         }
     }
 }
