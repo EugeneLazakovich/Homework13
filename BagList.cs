ea2485e7f61e60508dee79d15f583701f01eaa21
EugeneLazakovich <50924111+EugeneLazakovich@users.noreply.github.com>
2019-07-30 13:08:36 +0100

    fixfixfix

---
+++ BagList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13._1
+{
+    class BagList
+    {
+        List<Item> items = new List<Item>();
+
+        public BagList()
+        {
+            items.Add(new MagicStick("Magic Stick", 10, 12, 6));
+            items.Add(new WindLace("Wind Lace", 8, 14, 9));
+            items.Add(new RingRegen("Ring of Regen", 6, 15, 10));
+            items.Add(new SageMask("Sage's Mask", 13, 10, 7));
+            items.Add(new BootsSpeed("Boots of Speed", 7, 15, 8));
+            items.Add(new GlovesHaste("Gloves of Haste", 7, 10, 13));
+            items.Add(new Cloak("Cloak", 10, 13, 4));
+            items.Add(new RingTarrasque("Ring of Tarrasque", 9, 10, 13));
+            items.Add(new RingHealth("Ring of Health", 10, 12, 9));
+            items.Add(new VoidStone("Void Stone", 7, 15, 11));
+                        
+        }
+        int bagCapacity = 6;
+        Random rnd = new Random();
+        public List<Item> getBag()
+        {
+            List<Item> bagList = new List<Item>();
+            for (int i = 0; i < bagCapacity; i++)
+            {
+                int rndNumb = rnd.Next(10);
+                bagList.Add(items[rndNumb]);
+            }
+            return bagList;
+        }
+    }
+}
---
+++ HeroList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13._1
+{
+    class HeroList : ICloneable
+    {
+        Dictionary<Hero, List<Item>> team1 = new Dictionary<Hero, List<Item>>();
+        public HeroList()
+        {
+            team1.Add(new Doom("Doom", 23, 14, 31), new BagList().getBag());
+            team1.Add(new Huskar("Huskar", 15, 16, 35), new BagList().getBag());
+            team1.Add(new Medusa("Medusa", 16, 33, 19), new BagList().getBag());
+            team1.Add(new Viper("Viper", 16, 29, 20), new BagList().getBag());
+            team1.Add(new Lich("Lich", 33, 18, 20), new BagList().getBag());
+        }
+        public HeroList(Dictionary<Hero, List<Item>> team2)
+        {
+            team1 = team2;
+        }
+        public override string ToString()
+        {
+            string result = "";
+            foreach (KeyValuePair<Hero, List<Item>> dd in team1)
+            {
+                Console.WriteLine(dd.Key);
+                foreach (Item ddValue in dd.Value)
+                {
+                    Console.WriteLine("          " + ddValue);
+                }
+            }
+            return result;
+        }
+        public object Clone()
+        {
+            List<Item> cloneList = new List<Item>();
+            Dictionary<Hero, List<Item>> clonDic = new Dictionary<Hero, List<Item>>();
+
+
+            foreach (KeyValuePair<Hero, List<Item>> dd in team1)
+            {
+                foreach (Item d in dd.Value)
+                {
+                    cloneList.Add(d);
+                }
+                clonDic.Add(dd.Key, dd.Value);
+            }
+            HeroList clonCollection = new HeroList(clonDic);
+
+            return clonCollection;
+        }
+    }
+}
